fbdb5524b77c26e6fdc93af3791b093bc3ba9d0f
MikosJ <56031072+MikosJ@users.noreply.github.com>
2023-11-22 19:33:44 +0100

    implementation

--- src/main/java/com/example/motpapp/Controller/OTPController.java
+++ src/main/java/com/example/motpapp/Controller/OTPController.java
@@ -1,5 +1,6 @@
 package com.example.motpapp.Controller;
 
+import com.example.motpapp.CredentialRepository;
 import com.example.motpapp.DTO.ValidateCodeDto;
 import com.example.motpapp.model.Validation;
 import com.google.zxing.BarcodeFormat;
@@ -16,13 +17,16 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/code")
-public class OTPController {
+public class OTPController{
 
     private final GoogleAuthenticator gAuth;
+    private final CredentialRepository credentialRepository;
 
     @SneakyThrows
     @GetMapping("/generate/{username}")
@@ -31,7 +35,7 @@ public class OTPController {
 
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
 
-        String otpAuthURL = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL("demo", username, key);
+        String otpAuthURL = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL("my-demo", username, key);
 
         BitMatrix bitMatrix = qrCodeWriter.encode(otpAuthURL, BarcodeFormat.QR_CODE, 200, 200);
 
@@ -43,5 +47,22 @@ public class OTPController {
     @PostMapping("/validate/key")
     public Validation validateKey(@RequestBody ValidateCodeDto body) {
         return new Validation(gAuth.authorizeUser(body.getUsername(), body.getCode()));
+    }
+
+    @GetMapping("/scratches/{username}")
+    public List<Integer> getScratches(@PathVariable String username) {
+        return getScratchCodes(username);
+    }
+
+    private List<Integer> getScratchCodes(@PathVariable String username) {
+        return credentialRepository.getUser(username).getScratchCodes();
+    }
+
+    @PostMapping("/scratches/")
+    public Validation validateScratch(@RequestBody ValidateCodeDto body) {
+        List<Integer> scratchCodes = getScratchCodes(body.getUsername());
+        Validation validation = new Validation(scratchCodes.contains(body.getCode()));
+        scratchCodes.remove(body.getCode());
+        return validation;
     }
 }
